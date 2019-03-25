cd4ef24b933440e08d41f5b4b01b66df7f41f6d0
manjumugali <manjumugali111@gmail.com>
2019-03-25 21:43:43 +0530

    codedeploy

---
+++ scripts/installDependecies.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install python3-pip python3-dev nginx git -y
+sudo apt-get update
+sudo pip3 install virtualenv -y
+virtualenv venv
+source venv/bin/activate
+git clone https://github.com/manjumugali/ChatApp-Django.git
+cd ChatApp-Django/
+pip3 install -r requirements.txt
+pip3 install django bcrypt django-extensions
+pip3 install gunicorn
+python3 manage.py collectstatic
+gunicorn --bind 0.0.0.0:8000 ChatApp-Django.wsgi:application
+sudo exit
+sudo mv /conffile/gunicorn.service/ /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
+
+sudo mv /conffile/ChatApp-Django /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/ChatApp-Django /etc/nginx/sites-enable
+sudo nginx -t
+sudo rm /etc/nginx/sites-enabled/default
+sudo service nginx restart
