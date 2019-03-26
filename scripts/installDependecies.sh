5ef5515bae1d04240cced9d69d8777a9778fa4eb
Manjunath <35911822+manjumugali@users.noreply.github.com>
2019-03-27 01:50:35 +0530

    Update installDependecies.sh

--- scripts/installDependecies.sh
+++ scripts/installDependecies.sh
@@ -11,11 +11,3 @@ cd ChatApp-Django/
 pip3 install -r requirements.txt
 pip3 install django bcrypt django-extensions
 pip3 install gunicorn
-sudo cp files/gunicorn.service /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl start gunicorn
-sudo systemctl enable gunicorn
-sudo cp files/ChatApp-Django /etc/nginx/sites-available/
-sudo ln -s /etc/nginx/sites-available/ChatApp-Django /etc/nginx/sites-enabled/
-sudo nginx -t
-sudo rm /etc/nginx/sites-enabled/default
