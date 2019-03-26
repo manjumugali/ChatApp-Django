3c94845852e3125cae1c86b99976455a17a7c602
Manjunath <35911822+manjumugali@users.noreply.github.com>
2019-03-26 09:37:42 +0530

    Update installDependecies.sh

--- scripts/installDependecies.sh
+++ scripts/installDependecies.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo apt-get install python3-pip python3-dev nginx git -y
 sudo apt-get update
-sudo pip3 install virtualenv -y
+sudo pip3 install virtualenv
 virtualenv venv
 source venv/bin/activate
 git clone https://github.com/manjumugali/ChatApp-Django.git
