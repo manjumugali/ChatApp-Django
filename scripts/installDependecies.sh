2d8bcf71e66db5c44f2d5ef78393732c154cbeb3
Manjunath <35911822+manjumugali@users.noreply.github.com>
2019-03-30 03:05:03 +0530

    Update installDependecies.sh

--- scripts/installDependecies.sh
+++ scripts/installDependecies.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install python3-pip python3-dev git -y
+sudo apt-get install python3-pip python3-dev nginx git -y
 sudo apt-get update
 sudo pip3 install virtualenv
 virtualenv --python=python3 /home/ubuntu/venv
