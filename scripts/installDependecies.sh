8ae98a4598ec99a263db28c66d3aadfaf1a5e7e6
Manjunath <35911822+manjumugali@users.noreply.github.com>
2019-03-30 00:14:08 +0530

    Update installDependecies.sh

--- scripts/installDependecies.sh
+++ scripts/installDependecies.sh
@@ -4,7 +4,12 @@ sudo apt-get update
 sudo apt-get install python3-pip python3-dev nginx git -y
 sudo apt-get update
 sudo pip3 install virtualenv
-virtualenv venv
-source venv/bin/activate
-pip3 install gunicorn
+virtualenv --python=python3 /home/ubuntu/venv
+source /home/ubuntu/venv/bin/activate
+cd /home/ubuntu/ChatApp-Django/
+pip3 install -r requirements.txt
+pip3 install django bcrypt django-extensions
+python3 manage.py collectstatic
+gunicorn --bind 0.0.0.0:8000 ChatApp-Django.wsgi:application
+
 
