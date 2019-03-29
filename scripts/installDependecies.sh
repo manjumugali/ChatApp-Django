b4ba1beff1aea3ed752e4df363fee5b6eb167c64
Manjunath <35911822+manjumugali@users.noreply.github.com>
2019-03-30 01:02:38 +0530

    Update installDependecies.sh

--- scripts/installDependecies.sh
+++ scripts/installDependecies.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install python3-pip python3-dev nginx git -y
+sudo apt-get install python3-pip python3-dev git -y
 sudo apt-get update
 sudo pip3 install virtualenv
 virtualenv --python=python3 /home/ubuntu/venv
@@ -11,6 +11,5 @@ pip3 install -r requirements.txt
 pip3 install django bcrypt django-extensions
 sudo chmod 777 manage.py
 python3 manage.py collectstatic
-sudo rm -rf /etc/nginx/sites-available/default
 
 
