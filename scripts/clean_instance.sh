3d89a8a6e7cd040f8bd01fe6bfa2d3e4e2b230b3
Manjunath <35911822+manjumugali@users.noreply.github.com>
2019-04-01 13:10:30 +0530

    Update clean_instance.sh

--- scripts/clean_instance.sh
+++ scripts/clean_instance.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo systemctl stop gunicorn
+sudo systemctl stop nginx
 sudo rm -rf /home/ubuntu/*
 sudo rm -rf /etc/systemd/system/gunicorn.service
 sudo rm -rf /etc/nginx/sites-available/ChatApp-Djnago
