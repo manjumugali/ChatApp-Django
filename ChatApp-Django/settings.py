5c0f06dc5689caaf3f2a0f0b05313d6c5cba56f3
Manjunath <35911822+manjumugali@users.noreply.github.com>
2019-04-03 11:50:10 +0530

    Update settings.py

--- ChatApp-Django/settings.py
+++ ChatApp-Django/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'NAME': 'chatapp',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': '**************',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
